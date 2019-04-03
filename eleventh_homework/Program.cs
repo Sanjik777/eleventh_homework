78dab2b977677ac3ecdd1983288d4bde5cc71a18
Sanjik <Beiseev_193@mail.ru>
2019-04-03 20:42:29 +0600

    v1

---
+++ eleventh_homework/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+/*
+Использование коллекций.
+
+1.	Создать коллекцию List <int> . Вывести на экран позицию и значение элемента,
+являющегося вторым максимальным значением в коллекции.
+Вывести на экран сумму элементов на четных позичиях.
+
+*/
+namespace eleventh_homework
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			const int penultIndex = 2;
+
+			Console.WriteLine("------------list:------------\n");
+			List<int> ourList = new List<int>() {6,22,8,2,9,7 };
+			foreach(var i in ourList)
+			{
+				Console.Write(i+" ");
+			}
+
+			// Найдем сумму четных
+
+			int sumEven = 0;
+
+			for (int i = 0; i < ourList.Count; i++)
+			{
+				if (i % 2 != 0)
+				{
+					sumEven += ourList[i];
+				}
+			}
+
+			// Отсортируем, потом найдем второе максимальное значение
+
+			// Первый способ сортировки - Sort()
+			Console.WriteLine("\n\n-----Sorted through Sort()------\n");
+			List<int> listSort = ourList;
+			listSort.Sort();
+
+			foreach (var i in listSort)
+			{
+				Console.Write(i + " ");
+			}
+			Console.WriteLine($"\nsecond max element: {listSort.Find(x=>x== listSort[listSort.Count - penultIndex])}");
+
+
+			// Второй способ сортировки - OrderBy()
+			Console.WriteLine("\n\n-----Sorted through OrderBy()-------\n");
+			List<int> listOrderBy = ourList.OrderBy(x=>x).ToList();
+			foreach (var i in listOrderBy)
+			{
+				Console.Write(i + " ");
+			}
+			Console.WriteLine($"\nsecond max element:" +
+				$" {listOrderBy.Find(x => x == listOrderBy[listOrderBy.Count - penultIndex])}");
+
+
+			// Третий способ сортиовки - CompareTo(), потом преобразования в массив
+			Console.WriteLine("\n\n-----Sorted through CompareTo() and list transform to array-------\n");
+			
+			List<int>  ourSotredListbyComparison = ourList;
+
+			ourSotredListbyComparison.Sort((x, y) => x.CompareTo(y));
+
+			int[] arrayList = ourSotredListbyComparison.ToArray();
+
+			foreach (var i in arrayList)
+			{
+				Console.Write(i + " ");
+			}
+			Console.WriteLine($"\nsecond max element:" +
+				$" {arrayList.GetValue(  arrayList.Length - penultIndex)  }");
+
+			Console.WriteLine($"\n\nSum of elements in even positions : {sumEven}");
+			Console.ReadKey();
+		}
+	}
+}
---
+++ practical_work/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/*
+ Использование коллекций.
+
+1.	Удалить все нечетные элементы списка ArrayList
+2.	Дана коллекция. Вывести на экран элементы,
+значение которых больше среднего арифметического всех элементов коллекции.
+ */
+namespace practical_work
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("-----first list-----\n");
+
+			ArrayList list = new ArrayList {"Ivanov",22, 13000, "list", "222", "ZXC"};
+			foreach(var i in list)
+			{
+				Console.Write(i + " ");
+			}
+			Console.WriteLine("\n\nDeleted elements in odd positions:");
+			for(int i = 1; i < list.Count; i+=1)
+			{
+				list.RemoveAt(i);
+			}
+			foreach (var i in list)
+			{
+				Console.Write(i + " ");
+			}
+
+			List<int> nextList = new List<int> {1,22,44,5,9,0,2,3,4 };
+			Console.WriteLine("\n\n-----second list:------\n");
+
+			foreach (var i in nextList)
+			{
+				Console.Write(i + " ");
+			}
+
+			int averageNumerator = 0;
+			int averageDenominator = 0;
+
+			for (int i = 0; i < nextList.Count; i++)
+			{
+				averageNumerator += nextList[i];
+				averageDenominator += 1;
+			}
+			int average = averageNumerator/ averageDenominator;
+			List<int> biggerThenAverage = nextList.FindAll(x=>x> average);
+			Console.Write("\n\nElements which bigger then average: ");
+			foreach (var i in biggerThenAverage)
+			{
+				Console.Write(i + " ");
+			}
+			Console.ReadKey();
+		}
+	}
+}
